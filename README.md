0d326df7e5077746a81be059a7163791fdbe47e9
s0r00t <143@s0r00t.net>
2015-01-30 22:13:48 +0100

    More work part 2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 IRCMachine is my own Python IRC bot made to help with development related channels.
 This is a Python 2.7 version, although I could make a Py3 version.
-
+It's based on [http://docs.python-requests.org/en/latest/](requests) and [https://bitbucket.org/jaraco/irc](irc) libs.
 
 ## Installation
 After cloning, run those commands (you may need admin rights to run those) :
@@ -10,11 +10,11 @@ After cloning, run those commands (you may need admin rights to run those) :
 
 ## Usage
 
-Run main.py in your favourite interpreter :
+First, you must configure the bot via ircmachine.json.
+Check wiki for help.
+Then, run main.py with your favourite interpreter :
 >python main.py
 
 ## Commands list
 
->.gh [issue|PR]
-
-Gives the title and the link of an issue/a PR.
+[https://github.com/s0r00t/IRCMachine/wiki/Commands-list](Check this wiki page for commands list).
--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-import irc
+import irc.bot
 import json
 import requests
+import sys
+from fncts import *
 
-def stLog(type, msg):
-    if type in ["INFO","WARN","ERROR","FATAL"]:
-        print("["+type+"] "+msg)
-    else:
-        print("[FATAL] Error while trying to log. Pretty paradoxal, isn't it?")
+class IRCMachine(irc.bot.SingleServerIRCBot):
+     def __init__(self, chans, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nick, nick)
 
-GHApi = "https://api.github.com"
-GHRaw = "https://raw.githubusercontent.com"
-cfgParsed = False
+def main():
+    GHApi = "https://api.github.com"
+    GHRaw = "https://raw.githubusercontent.com"
+    #to gen token : https://github.com/settings/tokens/new
+    #use public_repo and gist
+    cfgJson = None
+    while cfgJson is None:
+        try:
+            stLog("INFO","Parsing config file...")
+            with open("ircmachine.json","r") as file:
+                cfgJson = json.load(file)
+            stLog("INFO","Config parsed.")
+        except IOError:
+            stLog("WARN","No config file found. Fetching latest one from repo...")
+            cfgReq = requests.get(GHRaw+"/s0r00t/IRCMachine/master/ircmachine.json")
+            with open("ircmachine.json","w") as file:
+                file.write(cfgReq.text)
+            stLog("INFO","Restarting config parse.")
 
-while cfgParsed != True:
-    try:
-        stLog("INFO","Parsing config file...")
-        with open("ircmachine.json","r") as file:
-            cfgJson = json.load(file)
-        stLog("INFO","Config parsed.")
-        cfgParsed = True
-    except IOError:
-        stLog("WARN","No config file found. Fetching latest one from repo...")
-        cfgReq = requests.get(GHRaw+"/s0r00t/IRCMachine/master/ircmachine.json")
-        with open("ircmachine.json","w") as file:
-            file.write(cfgReq.text)
-        stLog("INFO","Restarting config parse.")
+    stLog("INFO","IRCMachine started.")
+    #TODO : this is the check for important fields. ADD MORE FIELDS
+    for i in ["nick","server"]:
+        if not i in cfgJson:
+            stLog("FATAL","No \'"+i+"\' field in the config file. IRCMachine cannot run without \'"+i+"\'   field defined.")
+            stLog("INFO","IRCMachine stopped.")
+            sys.exit(1)
+        else:
+            stLog("INFO","\'"+i+"\' : "+cfgJson[i])
 
-stLog("INFO","IRCMachine started.")
-stLog("INFO","Owner : "+cfgJson["owner"])
+if __name__ == "__main__":
+    main()
