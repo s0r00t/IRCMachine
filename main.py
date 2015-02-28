8c71b28e3660b32edc03fec1b5ef05ac9af76dcd
s0r00t <143@s0r00t.net>
2015-03-01 00:46:27 +0100

    Added command system, and -t arg for token

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 import irc.bot
 import json
 import requests
@@ -5,10 +6,8 @@ import sys
 import os
 
 quiet = False
-cfgPath = "ircmachine.json"
 
-class LogError(Exception):
-    pass
+class LogError(Exception):pass
 
 def stLog(type, msg):
     """
@@ -36,30 +35,48 @@ class IRCMachine(irc.bot.SingleServerIRCBot):
         stLog("WARN","Nick in use. Using temp one.")
         c.nick(c.get_nickname() + "_")
 
+    def on_pubmsg(self, c, e):
+        if e.arguments[0].startswith(self.cfgJson["cmd"]):
+            self.runCmd(e.arguments[0].split(self.cfgJson["cmd"],1)[1].split(' '),e)
+
+    def runCmd(self,cmdArray,e):
+        #cmdArray[0] : command
+        #cmdArray[>0] : arg
+        stLog("INFO","User '"+e.source.nick+"' sent command '"+cmdArray[0]+"'.")
+        c = self.connection
+        if cmdArray[0] == "hello":
+            c.privmsg(e.target,"Hello World from "+e.source.nick+"!")
+
+
     def on_welcome(self, c, e):
         stLog("INFO","Connected! Joining channels...")
         for i in self.chans:
             c.join(i)
             stLog("INFO","Joined \'"+i+"\'.")
         #TODO : DO SOMETHING WITH THAT API
-        if self.cfgJson['gh-token']:
+        if 'argToken' in globals():
+            stLog("INFO","Using GitHub token to connect to the API...")
+            GHApiLog = requests.get('https://api.github.com/user', auth=(argToken, 'x-oauth-basic'))
+        elif self.cfgJson['gh-token']:
             stLog("INFO","Using GitHub token to connect to the API...")
             GHApiLog = requests.get('https://api.github.com/user', auth=(self.cfgJson['gh-token'], 'x-oauth-basic'))
-            if GHApiLog.status_code == 200:
-                stLog("INFO","Logged to GitHub API!")
-            else:
-                stLog("WARN","Failed to log into GitHub API. Perhaps the token is not working?")
         else:
             stLog("WARN","No GitHub token provided. There may be limitations when using GitHub API.")
 
+        if GHApiLog.status_code == 200:
+            stLog("INFO","Logged to GitHub API!")
+        else:
+            stLog("WARN","Failed to log into GitHub API. Perhaps the token is not working?")
+
 def main():
-    for i in sys.argv:
+    cfgPath = "ircmachine.json"
+    #TODO : MORE ARGUMENTS
+    for i in sys.argv[1:]:
         if i=="main.py":pass
         elif i == "-h":
-            print("IRCMachine help\n-h: Show this message\n-q: Quiet mode (no INFO or WARN)\n-c <file>: use <file> as config file")
+            print("IRCMachine help\n-h: Show this message\n-q: Quiet mode (no INFO or WARN)\n-c <file>: use <file> as config file\n-t <token>: use <token> as GitHub API token")
             sys.exit(1)
         elif i == "-q":
-            global quiet
             quiet = True
         elif i == "-c":
             if not "ircmachine.json" in sys.argv[sys.argv.index(i)+1] and not os.path.isfile(sys.argv[sys.argv.index(i)+1]+"/ircmachine.json"):
@@ -69,9 +86,12 @@ def main():
                 cfgPath = sys.argv[sys.argv.index(i)+1]+"/ircmachine.json"
             else:
                 cfgPath = sys.argv[sys.argv.index(i)+1]
+        elif i == "-t":
+            global argToken
+            argToken = sys.argv[sys.argv.index(i)+1]
 
     cfgJson = None
-    while cfgJson is None:
+    while cfgJson == None:
         try:
             stLog("INFO","Parsing config file...")
             with open(cfgPath,"r") as file:
@@ -95,7 +115,12 @@ def main():
             stLog("INFO","\'"+i+"\' : "+cfgJson[i])
 
     stLog("INFO","IRCMachine started.")
-    IRCMachine(cfgJson["chans"], cfgJson["nick"], cfgJson["server"],cfgJson).start()
+    try:
+        IRCMachine(cfgJson["chans"], cfgJson["nick"], cfgJson["server"],cfgJson).start()
+    except UnicodeDecodeError:
+        #TODO : find wtf is that error
+        #seems to be a problem with clients
+        stLog("FATAL","Unknown error. Please restart IRCMachine.")
 
 if __name__ == "__main__":
     main()
